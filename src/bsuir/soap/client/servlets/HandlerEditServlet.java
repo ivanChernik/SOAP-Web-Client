df06fa1458326eea82bc0dee6d235d074d3efbac
Ivan Chernikau <Ivan Chernikau@IvanChernikau>
2016-03-31 12:41:47 +0300

    REST Client's code not refactoring prepare to thrift

--- src/bsuir/soap/client/servlets/HandlerEditServlet.java
+++ src/bsuir/soap/client/servlets/HandlerEditServlet.java
@@ -1,11 +1,19 @@
 package bsuir.soap.client.servlets;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
 
 import bsuir.soap.client.servlets.service.ServiceUtil;
 import webService.soap.handbook.HandBookJS;
@@ -14,6 +22,7 @@ import webService.soap.handbook.model.DefenitionModel;
 @WebServlet("/HandlerEditServlet")
 public class HandlerEditServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String USER_AGENT = "Mozilla/5.0";
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -24,15 +33,39 @@ public class HandlerEditServlet extends HttpServlet {
 				new String(request.getParameter("editNameDefenition").getBytes("ISO-8859-1"), "UTF-8"));
 		defenitionModel
 				.setBodyDefenition(new String(request.getParameter("editDefenition").getBytes("ISO-8859-1"), "UTF-8"));
-		HandBookJS handbook = ServiceUtil.getServiceSOAP().getPort(HandBookJS.class);
+		// HandBookJS handbook =
+		// ServiceUtil.getServiceSOAP().getPort(HandBookJS.class);
 
 		if (request.getParameter("add") != null) {
-			handbook.addDefenition(defenitionModel);
+			String str = new String("{\"value\": \"" + defenitionModel.getBodyDefenition() + "\", \"name\": \""
+					+ defenitionModel.getNameDefenition() + "\"}");
+
+			try {
+				URL url = new URL("http://192.168.137.34:8000/one/");
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+				connection.setRequestMethod("POST");
+				connection.setDoOutput(true);
+				connection.setRequestProperty("Content-Type", "application/json");
+				connection.setRequestProperty("User-Agent", USER_AGENT);
+				connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+				//connection.setConnectTimeout(5000);
+				//connection.setReadTimeout(5000);
+
+				DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+				out.writeBytes(str);
+				out.flush();
+				out.close();
+				System.out.println(connection.getResponseCode());
+
+			} catch (Exception e) {
+				System.out.println("\nError while calling Crunchify REST Service");
+				System.out.println(e);
+			}
 
 		} else if (request.getParameter("update") != null) {
-			handbook.updateDefenition(defenitionModel);
+			// handbook.updateDefenition(defenitionModel);
 		} else if (request.getParameter("delete") != null) {
-			handbook.deleteDefenition(defenitionModel.getNameDefenition());
+			// handbook.deleteDefenition(defenitionModel.getNameDefenition());
 		}
 		String redirectURL = "/SOAPWebClient/edit.html";
 		response.sendRedirect(redirectURL);
--- src/bsuir/soap/client/servlets/HandlerIndexServlet.java
+++ src/bsuir/soap/client/servlets/HandlerIndexServlet.java
@@ -1,8 +1,15 @@
 package bsuir.soap.client.servlets;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,6 +20,9 @@ import javax.servlet.http.HttpSession;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import bsuir.soap.client.servlets.service.ServiceUtil;
 import webService.soap.handbook.HandBookJS;
 import webService.soap.handbook.model.DefenitionModel;
@@ -20,23 +30,54 @@ import webService.soap.handbook.model.DefenitionModel;
 @WebServlet("/HandlerIndexServlet")
 public class HandlerIndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String USER_AGENT ="Mozilla/5.0";
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
 		String searchParameter = request.getParameter("defenition");
+		searchParameter.trim();	
 
-		HandBookJS handbook = ServiceUtil.getServiceSOAP().getPort(HandBookJS.class);
+		String defenitionName = null;
+		String bodyValue = null;
+		StringBuilder inputBuilder = new StringBuilder();
+		// Step2: Now pass JSON File Data to REST Service
+	try {
+			URL url = new URL("http://192.168.137.34:8000/one/"+searchParameter);
+			HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setRequestProperty("User-Agent", USER_AGENT);
+			connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
-		DefenitionModel defenition = handbook.searchDefenition(searchParameter);
+			BufferedReader in = new BufferedReader(new  InputStreamReader(connection.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				inputBuilder.append(line);
+			}
+			//in.reset();
+			in.close();
+
+			JSONObject jsonObject = new JSONObject(inputBuilder.toString());
+			
+			defenitionName = jsonObject.getString("name");
+			bodyValue = jsonObject.getString("value");
+			System.out.println(jsonObject);
+			
+		} catch (Exception e) {
+			System.out.println("\nError while calling Crunchify REST Service");
+			System.out.println(e);
+		}
 
 		HttpSession session = request.getSession();
 		response.setCharacterEncoding("UTF-8");
-		if (defenition != null) {
-			session.setAttribute("getDefenition", searchParameter + " - " + defenition.getBodyDefenition());
+		if (defenitionName != null ||  bodyValue != null) {
+			session.setAttribute("getDefenition", defenitionName + " - " + bodyValue);
 		} else {
-			session.setAttribute("getDefenition", searchParameter + " - не найдено");
+			session.setAttribute("getDefenition","не найдено");
 		}
 		String redirectURL = "/SOAPWebClient/";
 		response.sendRedirect(redirectURL);
